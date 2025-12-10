ba194480d0e720d7257a3658cfe326f09a73f161
Retro_chain <94081759+Retrochain@users.noreply.github.com>
2025-12-09 23:44:45 -0600

    Formatting Issues

--- main.py
+++ main.py
@@ -100,7 +100,9 @@ async def on_message(message):
     words = message.content.lower().split()
     if any(w in ["hi", "hey", "hello", "sup"] for w in words):
         await message.channel.send(random.choice(greetings))
-        return
+    else:
+        await message.channel.send(random.choice(replys)) 
+    return
    
    # Replies to the user when the bot is mentioned
    if(bot.user in message.mentions):
